b4106389e68fa1466b14efce1ddecbb4769ccefb
corganfuzz <gruizcavero@uh.edu>
2020-06-10 17:58:19 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AppContainer } from "./styles";
+import { Column } from "./components/Column";
+import { Card } from "./components/Card";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Column text="To do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn TS" />
+      </Column>
+      <Column text="Done">
+        <Card text="Use Static typing" />
+      </Column>
+    </AppContainer>
   );
-}
+};
 
 export default App;
